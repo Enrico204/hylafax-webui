1630689d4f68a4549ca1d1abcc41c12052165fa5
Enrico204 <enrico204@gmail.com>
2015-10-01 17:18:59 +0200

    Fixed bug on sent Fax (black background)

--- get.php
+++ get.php
@@ -11,14 +11,14 @@ switch($_GET["queue"]) {
         $fileq = HYLAFAX_ROOT . "docq/doc" . $qid . ".tif." . $qid;
         $filetif = HYLAFAX_ROOT . "docq/doc" . $qid . ".tif";
         if(file_exists($file)) {
-            passthru("/usr/bin/ps2pdf $file -");
+            passthru("/usr/bin/ps2pdf -sPAPERSIZE=a4 $file -");
         } else {
             if(file_exists($fileq)) {
                 $file = $fileq;
             } else {
                 $file = $filetif;
             }
-            system("/usr/bin/tiff2pdf -p A4 -o /tmp/fax_$qid.pdf $file");
+            system("/usr/bin/tiff2ps $file | /usr/bin/ps2pdf -sPAPERSIZE=a4 - /tmp/fax_$qid.pdf");
             echo file_get_contents("/tmp/fax_$qid.pdf");
             unlink("/tmp/fax_$qid.pdf");
         }
